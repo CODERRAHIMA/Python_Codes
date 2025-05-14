bba79fb551000186f7bcaca1b27310dbebbf168f
Rahima Akter <139994153+CODERRAHIMA@users.noreply.github.com>
2025-05-15 00:10:48 +0600

    07_string_slicing.py

--- python/07_string_slicing.py
+++ python/07_string_slicing.py
@@ -2,10 +2,10 @@
 # str[ staring_idx : ending_idx ]   - ending idx is not included
 # str[ i : j ]   will be print  from i to (j-1)
 
-name = "Raha"
-slice1 = name[0 : 3]  # first 3 idx = [0] [1] [2] = sha
+name = "Rahima"
+slice1 = name[0 : 3]  # first 3 idx = [0] [1] [2] = Rah
 print (slice1)
-slice2 = name[2 : 5]  # idx 2 3 4 = adh
+slice2 = name[2 : 5]  # idx 2 3 4 = him
 print (slice2)
 
 print (name[3 : len(name)])  # idx[3] to last
@@ -14,7 +14,7 @@ print (name[3: ])   # [3 : len(name)] = [3 : last]
 
 
 # Negative index in slicing
-# name = raha -  a=-1 h=-2 a=-3 r=-4 
+# name = rahima -  a=-1 m=-2 i=-3 h=-4 a=-5 R=-6 
 # str[-j : -i]  will be print  from -j to (-i-1)
-name3 = "raha"
+name3 = "rahima"
 print (name3[-4 : -1])
